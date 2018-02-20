8e2b2d0d7c6202b2218839aa179a2d5b473bf955
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-20 21:22:03 +0200

    final commit with int array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,68 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
+    private static final String RED = "\033[0;91m";
+    private static final String RESET = "\033[0m";
+
     public static void main(String[] args) {
-        
-        Queue queue = new Queue(10);
+        boolean programRunning = true;
+        System.out.println("How big do you want the queue?");
+        while (programRunning) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Queue size: ");
+
+            try {
+                int size = scanner.nextInt();
+                Queue queue = new Queue(size);
+
+                boolean running = true;
+
+                while (running) {
+                    String input = scanner.nextLine();
+
+                    switch (input) {
+                        case "e":
+                            System.out.print("Enqueue value: ");
+                            System.out.println(enq(queue, scanner));
+                            break;
+                        case "d":
+                            System.out.println(deq(queue));
+                            break;
+                        case "h":
+                            System.out.println(RED + "e <int arg>    = enqueue number given in <int arg>\n" +
+                                    "d              = dequeue next in line" + RESET);
+                            break;
+                        default:
+                            System.out.println(RED + "Invalid input: type h for help" + RESET);
+                            break;
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(RED + "INVALID INPUT" + RESET);
+            }
+        }
+    }
+
+    private static String enq(Queue queue, Scanner scanner) {
+        try {
+            Integer in = scanner.nextInt();
+            int res = queue.enqueue(in);
+            if (res == -40) {
+                return RED + "YOU MUST DEQUEUE FIRST!" + RESET;
+            }
+        } catch (InputMismatchException e) {
+            return RED + "INVALID INPUT" + RESET;
+        }
+        return queue.toString();
+    }
+
+    private static String deq(Queue queue) {
+        if (queue.dequeue() == -50) {
+            return RED + "YOU MUST ENQUEUE FIRST!" + RESET;
+        }
+        return queue.toString();
     }
 }
 
--- src/Queue.java
+++ src/Queue.java
@@ -1,32 +1,79 @@
+import java.util.Arrays;
+
 public class Queue {
 
     private int[] vector;
-    private int H;//head - beginning
-    private int T;//tail - end
+    private static int H;//head - beginning
+    private static int T;//tail - end
+    private StringBuilder qsb;
 
     Queue(int size) {
         vector = new int[size];
+        qsb = new StringBuilder();
         H = 0;
         T = 0;
     }
 
-    public int enqueue(int input) { //insert
+    public int enqueue(int input) { //add to queue
         if (T == vector.length) {
-            return -404;
+            if (H == 0) {
+                return -40;
+            } else {
+                vector[0] = input;
+                return T = 1;
+            }
         } else {
-            vector[T] = input;
-            return vector[T++];
+            if (T < H) {
+                if (T + 1 == H) {
+                    return -40;
+                } else {
+                    if (T == 0) {
+                        return vector[++T] = input;
+                    } else {
+                        return vector[T++] = input;
+                    }
+                }
+            } else {
+                return vector[T++] = input;
+            }
         }
     }
 
-    public int dequeue() { //delete
-        if (H == T) {
-            return -504;
+    public int dequeue() { //take turn
+        if (H == vector.length) {
+            if (T == 0 || T == 1) {
+                return -50;
+            } else return H = 1;
         } else {
-            H++;
-            return H;
+            if (H + 1 == T) {
+                return -50;
+            } else {
+                return ++H;
+            }
         }
     }
 
-   //override tosring
+    @Override
+    public String toString() {
+        qsb.delete(0, qsb.length());
+        if (H < T) {
+            for (int i = 0; i < H; ++i) {
+                qsb.append("\033[0;32m").append(vector[i]).append("\033[0m").append(" ");
+            }
+            for (int i = H; i < vector.length; ++i) {
+                qsb.append("\033[0;91m").append(vector[i]).append("\033[0m").append(" ");
+            }
+        } else {
+            for (int i = 0; i < T; ++i) {
+                qsb.append("\033[0;91m").append(vector[i]).append("\033[0m").append(" ");
+            }
+            for (int i = T; i < H; ++i) {
+                qsb.append("\033[0;32m").append(vector[i]).append("\033[0m").append(" ");
+            }
+            for (int i = H; i < vector.length; ++i) {
+                qsb.append("\033[0;91m").append(vector[i]).append("\033[0m").append(" ");
+            }
+        }
+        return qsb.toString();
+    }
 }
