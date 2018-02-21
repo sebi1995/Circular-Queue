34ba5dc81600a2847c3e0d02cc2e5fcacb28eb28
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-21 23:19:24 +0200

    final upload, program finished

--- src/Queue.java
+++ src/Queue.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Queue {
-
     private ArrayList<String> vector;
+    private ArrayList<String> colours;
     private int H;//head - beginning
     private int T;//tail - end
     private int HT = 0;
@@ -11,11 +11,12 @@ public class Queue {
     private StringBuilder qsb;
 
     Queue(int size) {
-        this.vector = new ArrayList<>(size);
+        vector = new ArrayList<>(size);
+        colours = new ArrayList<>(size);
         this.size = size;
-        this.qsb = new StringBuilder();
-        this.H = 0;
-        this.T = 0;
+        qsb = new StringBuilder();
+        H = -1;
+        T = -1;
 
         popEmptyQueue(size);
 
@@ -24,73 +25,58 @@ public class Queue {
     private void popEmptyQueue(int size) {
         for (int i = 0; i < size; ++i) {
             vector.add(" ");
+            colours.add(" ");
         }
     }
 
     public int enqueue(String input) { //add to queue
-        if (T == size) {
-            if (H == 0) {
+        if (T == size - 1) {
+            if (H == -1 || H == 0) {
                 return -40;
             } else {
-                vector.set(0, input);
-                T = 1;
+                T = 0;
+                vector.set(T, input);
+                colours.set(T, "r");
+                return 0;
             }
-        } else {
-            if (T < H) {
-                if (T + 1 == H) {
-                    return -40;
-                } else {
-                    if (T == 0) {
-                        T++;
-                        vector.set(T, input);
-                    } else {
-                        vector.set(T, input);
-                        ++T;
-                    }
-                }
-            } else {
-                vector.set(T++, input);
-            }
-        }
+        } else if (T + 1 == H) return -40;
+
+        vector.set(++T, input);
+        colours.set(T, "r");
+
         return 0;
     }
 
     public int dequeue() { //take turn
-        if (H == size) {
-            if (T == 0 || T == 1) {
+        if (H == -1 && T == -1) return -50;
+        if (H == size - 1) {
+            if (T == 0) {
                 return -50;
             } else {
-                H = 1;
+                H = -1;
             }
-        } else {
-            if (H + 1 == T) {
-                return -50;
-            } else {
-                ++H;
-                ++HT;
-            }
-        }
+        } else if (H + 1 == T) return -50;
+
+        colours.set(++H, "g");
+
         return 0;
     }
 
     @Override
     public String toString() {
         qsb.delete(0, qsb.length());
-        if (H < T) {
-            for (int i = 0; i < H; ++i)
-                qsb.append("\033[0;32m").append(vector.get(i)).append("\033[0m").append(" ");
-            for (int i = H; i < T; ++i)
-                qsb.append("\033[0;91m").append(vector.get(i)).append("\033[0m").append(" ");
-            for (int i = T; i < size; ++i)
-                qsb.append("\033[0;32m").append(vector.get(i)).append("\033[0m").append(" ");
-        } else {
-            for (int i = 0; i < T; ++i)
-                qsb.append("\033[0;91m").append(vector.get(i)).append("\033[0m").append(" ");
-            for (int i = T; i < H; ++i)
-                qsb.append("\033[0;32m").append(vector.get(i)).append("\033[0m").append(" ");
-            for (int i = H; i < size; ++i)
-                qsb.append("\033[0;91m").append(vector.get(i)).append("\033[0m").append(" ");
+
+        for (int i = 0; i < size; ++i) {
+            switch (colours.get(i)) {
+                case "r":
+                    qsb.append("\033[0;91m").append(vector.get(i)).append("\033[0m").append(" ");
+                    break;
+                case "g":
+                    qsb.append("\033[0;32m").append(vector.get(i)).append("\033[0m").append(" ");
+                    break;
+            }
         }
+
         return qsb.toString();
     }
 }
